51908dcd166abc229f84f9bf908837021ef3e30e
Steven Frieson <sfrieson@gmail.com>
2017-01-08 18:47:15 -0500

    Fixed dev middleware conditional.

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 require('dotenv').config();
 const app = express();
 
-if (!process.env.DEVELOPMENT) {
+if (process.env.DEVELOPMENT) {
   var morgan = require('morgan');
   app.use(morgan('dev'));
 }
